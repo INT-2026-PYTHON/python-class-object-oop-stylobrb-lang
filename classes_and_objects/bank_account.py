94400e1a8b5688ff112c80bf259ba37db8174842
Bibek R Behera <stylobrb@gmail.com>
2026-06-11 15:15:47 +0530

    Assignment 11-06-2026

--- classes_and_objects/bank_account.py
+++ classes_and_objects/bank_account.py
@@ -83,3 +83,42 @@ Explanation:
 =================================================
 
 """
+class BankAccount:
+    def __init__(self, name, account_number, opening_balance=0):
+        if opening_balance < 0:
+             print(f"opening balancee must be >= 0. setting balance to 0.")
+             opening_balance = 0
+        self.name = name
+        self.account_number = account_number
+        self.balance = opening_balance
+
+    def deposit(self, amount):
+        if amount <= 0:
+            print(f"Deposit amount must be > 0(got{amount})")
+        else:
+            self.balance += amount
+
+    def withdraw(self, amount):
+        if amount <= 0:
+            print(f"Deposit amount must be > 0(got{amount})")
+        elif amount > self.balance:
+            print(f"Insufficient funds for {self.name} (balance = {self.balance}, asked = {amount})")
+        else:
+            self.balance -= amount
+
+    def get_balance(self):
+        return self.balance
+    def __str__(self):
+        return f"Account[{self.account_number} - {self.name}]: ${self.balance}"
+
+a1 = BankAccount("Alice", "001", 500)   
+a2 = BankAccount("Bob", "002")
+a1.deposit(200)
+a1.withdraw(100)
+a1.withdraw(2000)
+a2.deposit(-50)
+a2.deposit(300)
+
+print(a1) 
+print(a2)    
+                          
